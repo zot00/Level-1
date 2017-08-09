c07c6832056d1b9b5485fe38694228218f9a8c9d
Lucas Baizer <titaniumsapphire32@gmail.com>
2017-08-09 11:56:11 -0700

    tried to make simon says and finished drum kit

---
+++ src/SimonSaysRecipe.java
@@ -0,0 +1,94 @@
+import java.awt.Component;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.net.URL;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Random;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import java.io.IOException;
+public class SimonSaysRecipe extends KeyAdapter {
+ 
+ // Complete steps 1 - 7 before you test
+ // 1. Make a JFrame variable 
+	JFrame x = new JFrame(); 
+ 	HashMap<Integer,String> images = new HashMap<Integer, String>();
+ 	private int imageIndex;
+ 	private int tries = 0;
+ 	private int simonSays = 0;
+ 	Date timeAtStart;
+ 	private  void makeAlbum() {
+ // 2. add 4 images which match keyboard keys like this: images.put(new Integer(KeyEvent.VK_UP), "image.jpg");
+  	images.put(new Integer(KeyEvent.VK_UP), "up.png");
+ // 3. Tell the user to "Press the matching key when 'Simon says' otherwise press a different key"
+  	JOptionPane.showMessageDialog(null, "press the matching key when 'Simon says' and otherwise press a different key.");																																				//Bushes of love song: 2: Hey how'd my father die? 1: 49 times - we fought that beast, your old man and me. It had a chicken head with duck feet and a woman's face, too. 2: aw that's rad. 1: and it was waiting in the bushes for us. Then, it ripped off your dad's face. He was screaming something awful. In fact, there was this huge mess and i had to change the floors. 2: the floors? 1: You see, his blood, it drained into the boards and i had to change'em, but we all got a chicken-duck-woman-thing waiting for us. Every day i worry all day. about what's waiting in the bushes for us. About what's waiting in the bushes of love. Every day i worry all day. about what's waiting in the bushes for us. About what's waiting in the bushes of love.Every day i worry all day. about what's waiting in the bushes for us. About what's waiting in the bushes of love. Every day i worry all day. about what's waiting in the bushes for us. About what's waiting in the bushes of love. 1: Yo, never knew my dad, he didn't care about me. Dead horizon is all my macro-binoculars see. moisture farming all my life and not a drop spilt. My aunt and uncle, double suns, I'm sippin' blue milk. My aunt and uncle, double suns, I'm sick of blue milk. But then, a desert hobo came and told me... 1: We've all got a chicken-duck-woman-thing waiting for us. Every day i worry all day. about what's waiting in the bushes for us. About what's waiting in the bushes of love. Every day i worry all day. about what's waiting in the bushes for us. About what's waiting in the bushes of love. Hold me when i open like a flower. Hold me right. Yeah, i haven't had to bake for a girl in a long time. a long time. 2: I think my cookin's awesome. 1: i got her picture in my photo wagon. 2: haha keep it poppin. 1: yeah, i'd bet she'd love to honky-tonk. 3: i'd bet she'd love to honky-tonk 1: that's what i said. 2: I used to glide across the desert you know i used glide on my speeder and pray that i dont find what i dont wanna find. hiding for me round the corner, no, no, no. waiting for us. i used to glide across the desert, 
+ // 4. call the method to show an image
+  	
+ 	}
+	public void keyPressed(KeyEvent e) {
+    	int keyCode = e.getKeyCode();
+    	// 16. make a points variable to track the score. tell the user their score at the end.
+    	//17. if the keyCode matches the imageIndex and "Simon says..."  increase their score
+    	//18.   if the keyCode doesn't match the imageIndex and "Simon didn't say..."  increase their score	
+    	//19. Use the speak method to tell the user if they were correct or not
+    	//13. increment tries by 1
+  	
+    	//14. if tries is greater than 9 (or however many you want)
+    	
+    	//15.    	exit the program
+
+    	//11. dispose of the frame
+   	
+    	//12. call the method to show an image
+
+	}
+	private void showImage() {
+    	//5. initialize your frame to a new JFrame()
+	 
+    	//6. set the frame to visible
+	
+  	 //frame.add(getNextRandomImage()); //7. rename to the name of your frame
+    	
+    	// 8. set the size of the frame 
+     	
+    	// 9. add a key listener to the frame
+	
+   	 //10. Use the speak method to either say "Simon says press this key" or "Press this key"
+    	//Hint: use the simonSays int and a random number
+	}
+	private Component getNextRandomImage() {
+    	this.imageIndex = new Random().nextInt(4) + 37;
+    	return loadImage(images.get(imageIndex));
+	}
+	private JLabel loadImage(String fileName) {
+    	URL imageURL = getClass().getResource(fileName);
+    	Icon icon = new ImageIcon(imageURL);
+    	return new JLabel(icon);
+	}
+  void speak(String words) {
+  	  try {
+  	   Runtime.getRuntime().exec("say " + words).waitFor();
+  	  } catch (Exception e) {
+  	   e.printStackTrace();
+  	  }
+   	}
+	public static void main(String[] args) throws Exception {
+	 new SimonSaysRecipe().makeAlbum();
+	}
+}
+
+/* 
+* 20. add a timer
+* ~~~ where the code starts running ~~~
+* timeAtStart = new Date();
+*
+* ~~~ where the code ends ~~~
+* Date timeAtEnd = new Date();
+* System.out.println((timeAtEnd.getTime()-timeAtStart.getTime())/1000);
+* System.exit(0);
+*/
+
--- src/drumkit.java
+++ src/drumkit.java
@@ -1,4 +1,86 @@
+import java.applet.AudioClip;
+import java.awt.GridLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.net.MalformedURLException;
+import java.net.URL;
 
-public class drumkit {
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JApplet;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class drumkit extends MouseAdapter {
+
+    JLabel drumLabelWithImage;
+
+    public static void main(String[] args) throws Exception {
+   	 new drumkit().getGoing();
+    }
+
+    private void getGoing() throws MalformedURLException {
+   	 // 1. Make a JFrame variable and initialize it using "new JFrame()"
+   	 JFrame x = new JFrame();
+   	 // 2. Make the frame visible
+   	 x.setVisible(true);
+   	 // 3. Set the size of the frame
+   	 x.setSize(1000, 1000);
+   	 // 4. Set the title of the frame
+   	 x.setTitle("Drum");
+   	 // 5. Make a JPanel and initialize it.
+   	 JPanel y = new JPanel();
+   	 // 6. Add the panel to the frame. (The panel is invisible.)
+    x.add(y);
+   	 // 7. Download an image of a drum from the Internet. Drop it into your Eclipse project under "default package".
+   	 // 8. Put the name of your image in a String variable.
+    String donChan = "donchan.jpg"; 
+   	 // 9. Edit the next line to use your String variable
+// drumLabelWithImage = createLabelImage(drumImageString);
+   	 drumLabelWithImage = createLabelImage(donChan);
+   	 // 10. Add the image to the panel
+     y.add(drumLabelWithImage);
+   	 // 11. Set the layout of the panel to "new GridLayout()"
+     y.setLayout(new GridLayout());
+   	 // 12. call the pack() method on the frame
+   	 x.pack();
+   	 // 13. add a mouse listener to drumLabelWithImage.
+   	 drumLabelWithImage.addMouseListener(this);
+    }
+
+    public void mouseClicked(MouseEvent e) {
+   	 // 14. When the mouse is clicked, print "mouse clicked"
+     System.out.println("mouse clicked");
+   	 JLabel drumClicked = (JLabel) e.getSource();
+   	 // 15. Download a drum sound and drop it into your "default package". You can find it on freesound.org. To download it, log in as leagueofamazing/code4life.
+   	 // 16. If they clicked on the drumImage...
+   	 if(drumClicked == drumLabelWithImage){
+   		 playSound("bum.wav");
+   	 }
+   		 // 17. ...use the playSound method to play a drum sound.
+   	 }
+   	 // 18. Add more images to make a drumkit. Remember to add a mouse listener to each one.
+
+    private JLabel createLabelImage(String fileName)
+			throws MalformedURLException
+	{
+		URL imageURL = getClass().getResource(fileName);
+		if (imageURL == null)
+		{
+			System.err.println("Could not find image " + fileName);
+			return new JLabel();
+		}
+		Icon icon = new ImageIcon(imageURL);
+		JLabel imageLabel = new JLabel(icon);
+		return imageLabel;
+	}
+
+
+    private void playSound(String fileName) {
+   	 AudioClip sound = JApplet.newAudioClip(getClass().getResource(fileName));
+   	 sound.play();
+    }
 
 }
