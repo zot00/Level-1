050cbcc76b309ee98094bc3646be36d4bbaa1bd9
Lucas Baizer <titaniumsapphire32@gmail.com>
2017-08-10 12:08:40 -0700

    made glitchy jeopardy and finished simon says

---
+++ src/Jeopardy.java
@@ -0,0 +1,203 @@
+ // Copyright Wintriss Technical Schools 2013
+import java.applet.AudioClip;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.GridLayout;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.URL;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.swing.BoxLayout;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JApplet;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+import java.io.File;
+
+/* This recipe is to be used with the Jeopardy Handout: http://bit.ly/1bvnvd4 */
+
+public class Jeopardy implements ActionListener {
+	private JButton firstButton;
+	private JButton secondButton;
+	private JButton thirdButton, fourthButton;
+	
+	private JPanel quizPanel;
+	int score = 0;
+	JLabel scoreBox = new JLabel("0");
+	int buttonCount = 0;
+
+	public static void main(String[] args) {
+		new Jeopardy().start();
+	}
+
+	private void start() {
+		JFrame frame = new JFrame();
+		quizPanel = new JPanel();
+		frame.setLayout(new BorderLayout());
+		
+		// 1. Make the frame show up
+		frame.setVisible(true);
+		// 2. Give your frame a title
+		frame.setTitle("Pokemon Jeopardy");
+		// 3. Create a JPanel variable to hold the header using the createHeader method
+		JPanel panel = new JPanel();
+		// 4. Add the header component to the quizPanel
+		quizPanel.add(panel);
+		// 5. Add the quizPanel to the frame
+		frame.add(quizPanel);
+		// 6. Use the createButton method to set the value of firstButton 
+		firstButton = createButton("100");
+	// 7. Add the firstButton to the quizPanel
+		quizPanel.add(firstButton);
+		// 8. Write the code inside the createButton() method below. Check that your game looks like Figure 1 in the Jeopardy Handout - http://bit.ly/1bvnvd4.
+		// 9. Use the secondButton variable to hold a button using the createButton method
+		secondButton=createButton("200");
+		// 10. Add the secondButton to the quizPanel
+		quizPanel.add(secondButton);
+		// 11. Add action listeners to the buttons (2 lines of code)
+		firstButton.addActionListener(this);
+		secondButton.addActionListener(this);
+		// 12. Fill in the actionPerformed() method below
+				
+		frame.pack();
+		quizPanel.setLayout(new GridLayout(buttonCount+1, 3));
+		frame.add(makeScorePanel(), BorderLayout.NORTH);
+		frame.setSize(Toolkit.getDefaultToolkit().getScreenSize().height, Toolkit.getDefaultToolkit().getScreenSize().width);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	/*
+	 * 13. Use the method provided to play the Jeopardy theme music 
+	 * 
+	 * 14. Add buttons so that you have $200, $400, $600, $800 and $1000 questions
+	 *
+	 * [optional] Use the showImage or playSound methods when the user answers a question 
+	 */
+	
+	private JButton createButton(String dollarAmount) {
+		// Create a new JButton
+		JButton button = new JButton();
+		// Set the text of the button to the dollarAmount
+		button.setText(dollarAmount);
+		// Increment the buttonCount (this should make the layout vertical)
+		buttonCount = buttonCount+1;
+		// Return your new button instead of the temporary button
+		return new JButton(dollarAmount);
+	}
+
+	public void actionPerformed(ActionEvent arg0) {
+		// Remove this temporary message:
+		JOptionPane.showMessageDialog(null,"pressed " + ((JButton)arg0.getSource()).getText() + " button");
+
+		// Use the method that plays the jeopardy theme music.
+		playJeopardyTheme();
+		JButton buttonPressed = (JButton) arg0.getSource();
+		// If the buttonPressed was the firstButton
+		if(buttonPressed == firstButton){
+			askQuestion("If Yveltal were to use its signature move, which Pokemon would it be most effective against? a - Beedrill, b - Scizor, c - Graveler, d - leavanny", "d", 100);
+			firstButton.setText("");
+		}
+			// Call the askQuestion() method
+			
+			// Fill in the askQuestion() method. When you play the game, the score should change.
+		
+		// Or if the buttonPressed was the secondButton
+		else if(buttonPressed == secondButton){
+			askQuestion("If you had a lvl 100, 1-health rapidash against a fully healed, asleep, lvl 100 snorlax, which move choice would be most effective to beat it in 2 turns? a - low kick, then headbutt; b - endeavor, then swift; c - double kick, then flare blitz; d - rest, then sleep talk", "b", 200);
+			secondButton.setText("");
+		}
+
+			// Call the askQuestion() method with a harder question
+		// Clear the button text (set the button text to nothing)
+	}
+
+	private void askQuestion(String question, String correctAnswer, int prizeMoney) {
+		// Remove this temporary message
+		JOptionPane.showMessageDialog(null, "this is where the question will be asked");
+		// Use a pop up to ask the user the question
+		String Panswer = JOptionPane.showInputDialog(question);
+		// If the answer is correct
+			if(Panswer == (correctAnswer)){
+				score = score+prizeMoney;
+				updateScore();
+				JOptionPane.showMessageDialog(null, "Correct!");
+			}else{
+				JOptionPane.showMessageDialog(null, "Incorrect... The actual answer was " + correctAnswer);
+				score = score - prizeMoney;
+				updateScore();
+			}
+			// Increase the score by the prizeMoney
+			// Call the updateScore() method
+			// Pop up a message to tell the user they were correct
+		// Otherwise
+			// Decrement the score by the prizeMoney
+			// Pop up a message to tell the user the correct answer
+			// Call the updateScore() method
+	}
+
+
+public void playJeopardyTheme() {
+		try {
+			AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("/Users/League/Google Drive/league-sounds/jeopardy.wav"));
+			Clip clip = AudioSystem.getClip();
+			clip.open(audioInputStream);
+			clip.start();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	private void playSound(String fileName) {
+		AudioClip scream = JApplet.newAudioClip(getClass().getResource(fileName));
+		scream.play();
+	}
+
+	private Component makeScorePanel() {
+		JPanel panel = new JPanel();
+		panel.add(new JLabel("score:"));
+		panel.add(scoreBox);
+		panel.setBackground(Color.CYAN);
+		return panel;
+	}
+
+	private void updateScore() {
+		scoreBox.setText("" + score);
+	}
+
+	private JPanel createHeader(String topicName) {
+		JPanel panelj = new JPanel();
+		panelj.setLayout(new BoxLayout(panelj, BoxLayout.PAGE_AXIS));
+		JLabel l1 = new JLabel(topicName);
+		l1.setAlignmentX(Component.CENTER_ALIGNMENT);
+		panelj.add(l1);
+		return panelj;
+	}
+
+	void showCorrectImage() {
+		showImage("correct.jpg");
+	}
+
+	void showIncorrectImage() {
+		showImage("incorrect.jpg");
+	}
+
+	private void showImage(String fileName) {
+		JFrame frame = new JFrame();
+		URL imageURL = getClass().getResource(fileName);
+		Icon icon = new ImageIcon(imageURL);
+		JLabel image = new JLabel(icon);
+		frame.add(image);
+		frame.setVisible(true);
+		frame.pack();
+	}
+}
--- src/SimonSaysRecipe.java
+++ src/SimonSaysRecipe.java
@@ -15,7 +15,7 @@ public class SimonSaysRecipe extends KeyAdapter {
  
  // Complete steps 1 - 7 before you test
  // 1. Make a JFrame variable 
-	JFrame x = new JFrame(); 
+	JFrame frame = new JFrame(); 
  	HashMap<Integer,String> images = new HashMap<Integer, String>();
  	private int imageIndex;
  	private int tries = 0;
@@ -24,41 +24,60 @@ public class SimonSaysRecipe extends KeyAdapter {
  	private  void makeAlbum() {
  // 2. add 4 images which match keyboard keys like this: images.put(new Integer(KeyEvent.VK_UP), "image.jpg");
   	images.put(new Integer(KeyEvent.VK_UP), "up.png");
+  	images.put(new Integer(KeyEvent.VK_DOWN), "down.png");
+  	images.put(new Integer(KeyEvent.VK_RIGHT), "right.png");
+  	images.put(new Integer(KeyEvent.VK_LEFT), "left.png");
  // 3. Tell the user to "Press the matching key when 'Simon says' otherwise press a different key"
   	JOptionPane.showMessageDialog(null, "press the matching key when 'Simon says' and otherwise press a different key.");																																				//Bushes of love song: 2: Hey how'd my father die? 1: 49 times - we fought that beast, your old man and me. It had a chicken head with duck feet and a woman's face, too. 2: aw that's rad. 1: and it was waiting in the bushes for us. Then, it ripped off your dad's face. He was screaming something awful. In fact, there was this huge mess and i had to change the floors. 2: the floors? 1: You see, his blood, it drained into the boards and i had to change'em, but we all got a chicken-duck-woman-thing waiting for us. Every day i worry all day. about what's waiting in the bushes for us. About what's waiting in the bushes of love. Every day i worry all day. about what's waiting in the bushes for us. About what's waiting in the bushes of love.Every day i worry all day. about what's waiting in the bushes for us. About what's waiting in the bushes of love. Every day i worry all day. about what's waiting in the bushes for us. About what's waiting in the bushes of love. 1: Yo, never knew my dad, he didn't care about me. Dead horizon is all my macro-binoculars see. moisture farming all my life and not a drop spilt. My aunt and uncle, double suns, I'm sippin' blue milk. My aunt and uncle, double suns, I'm sick of blue milk. But then, a desert hobo came and told me... 1: We've all got a chicken-duck-woman-thing waiting for us. Every day i worry all day. about what's waiting in the bushes for us. About what's waiting in the bushes of love. Every day i worry all day. about what's waiting in the bushes for us. About what's waiting in the bushes of love. Hold me when i open like a flower. Hold me right. Yeah, i haven't had to bake for a girl in a long time. a long time. 2: I think my cookin's awesome. 1: i got her picture in my photo wagon. 2: haha keep it poppin. 1: yeah, i'd bet she'd love to honky-tonk. 3: i'd bet she'd love to honky-tonk 1: that's what i said. 2: I used to glide across the desert you know i used glide on my speeder and pray that i dont find what i dont wanna find. hiding for me round the corner, no, no, no. waiting for us. i used to glide across the desert, 
  // 4. call the method to show an image
-  	
+  	showImage();
  	}
 	public void keyPressed(KeyEvent e) {
     	int keyCode = e.getKeyCode();
     	// 16. make a points variable to track the score. tell the user their score at the end.
+    	int points = 0;
     	//17. if the keyCode matches the imageIndex and "Simon says..."  increase their score
+    	if(keyCode == imageIndex){
+    		points = points + 1;
+    		speak("Correct!");
+    	}
     	//18.   if the keyCode doesn't match the imageIndex and "Simon didn't say..."  increase their score	
+    	else if(keyCode != imageIndex){
+    		points = points + 1;
+    		speak("Correct!");
+    	} else{
+    		points = points - 1;
+    		speak("Incorrect!");
+    	}
     	//19. Use the speak method to tell the user if they were correct or not
     	//13. increment tries by 1
-  	
+    	tries++;
     	//14. if tries is greater than 9 (or however many you want)
-    	
+    	if(tries>9){
+    		System.exit(0);
+    	}
     	//15.    	exit the program
 
     	//11. dispose of the frame
-   	
+    	frame.dispose();
     	//12. call the method to show an image
-
+    	showImage();
 	}
 	private void showImage() {
     	//5. initialize your frame to a new JFrame()
-	 
+		frame = new JFrame();
     	//6. set the frame to visible
-	
+		frame.setVisible(true);
   	 //frame.add(getNextRandomImage()); //7. rename to the name of your frame
-    	
+		frame.add(getNextRandomImage());
     	// 8. set the size of the frame 
-     	
+     	frame.setSize(1000, 1000);
     	// 9. add a key listener to the frame
-	
-   	 //10. Use the speak method to either say "Simon says press this key" or "Press this key"
+     	frame.addKeyListener(this);
+     	//10. Use the speak method to either say "Simon says press this key" or "Press this key"
     	//Hint: use the simonSays int and a random number
+        Random r = new Random();
+        simonSays = r.nextInt(2);
 	}
 	private Component getNextRandomImage() {
     	this.imageIndex = new Random().nextInt(4) + 37;
