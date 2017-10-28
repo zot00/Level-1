5a037193bd030265b5648b5e1d35059ed284f049
FruitWave <league@WTS8.attlocal.net>
2017-10-28 16:00:01 -0700

    almost finished wackamole, started simple calculator

--- src/Wackamole.java
+++ src/Wackamole.java
@@ -10,30 +10,39 @@ public class Wackamole implements ActionListener {
 	JPanel jp = new JPanel();
 	JFrame jf = new JFrame();
 	Random r = new Random();
-	int currentLoc;
+	static int currentLoc;
 	JButton buttons[] = new JButton[110];
 
 	public static void main(String[] args) {
 		Wackamole w = new Wackamole();
 		w.run();
+		w.whack();
 	}
-
-	public void run() {
-		currentLoc = r.nextInt(buttons.length);
-		drawButtons(currentLoc);
-	}
-
-	public void drawButtons(int rand) {
+	
+	void run() {
 		jf.add(jp);
-		jf.setVisible(true);
-		for (int i = 0; i < buttons.length; i++) {
+		for(int i = 0; i < buttons.length; i++) {
 			buttons[i] = new JButton();
 			jp.add(buttons[i]);
 			buttons[i].setVisible(true);
 			buttons[i].addActionListener(this);
 		}
 		jf.setSize(800, 400);
-		buttons[currentLoc].setText("(/*\\)");
+		jf.setVisible(true);
+		jp.setVisible(true);
+	}
+
+	public void whack() {
+		currentLoc = r.nextInt(buttons.length);
+		for (int i = 0; i < buttons.length; i++) {
+			if(i==currentLoc) {
+				buttons[i].setText("mole");
+			}
+			else {
+				buttons[i].setText("");
+			}
+		}
+		System.out.println(currentLoc);
 	}
 
 	void speak(String words) {
@@ -47,10 +56,12 @@ public class Wackamole implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		if (e.getSource() != buttons[currentLoc]) {
-			speak("Dork!");
-		} else {
-			speak("No! Don't touch the star-nosed mole's nose anymore! It's sensitiv!");
+		if(e.getSource() == buttons[currentLoc]) {
+			System.out.println("yes");
+			whack();
+		}
+		else {
+			System.out.println("no");
 		}
 	}
 }
