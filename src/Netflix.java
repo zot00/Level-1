ec6b23caea912ed0cdc9ca1805359b8fd7904a4f
RationalGeographic <league@WTS14.attlocal.net>
2018-01-06 16:01:35 -0800

    Finished Netflix

--- src/Netflix.java
+++ src/Netflix.java
@@ -1,9 +1,26 @@
 public class Netflix {
 	public static void main(String[] args) {
-		Movie BeeMovie = new Movie("BeeMovie", 3);
-		Movie EmojiMovie = new Movie("EmojiMovie", 2);
-		Movie LegoBatmanMovie = new Movie("LegoBatmanMovie", 3);
-		Movie LegoMovie = new Movie("LegoMovie", 4);
-		Movie LegoLNinjagoMovie = new Movie("LegoNinjagoMovie", 3);
+		NetflixQueue netflixQueue = new NetflixQueue();
+		Movie BeeMovie = new Movie("Bee Movie", 3);
+		Movie EmojiMovie = new Movie("Emoji Movie", 2);
+		Movie LegoBatmanMovie = new Movie("Lego Batman Movie", 3);
+		Movie LegoMovie = new Movie("Lego Movie", 4);
+		Movie LegoNinjagoMovie = new Movie("Lego Ninjago Movie", 3);
+		System.out.println("Cost for movie: " + LegoNinjagoMovie.getTicketPrice());
+		System.out.println("Cost for movie: " + LegoMovie.getTicketPrice());
+		System.out.println("Cost for movie: " + LegoBatmanMovie.getTicketPrice());
+		System.out.println("Cost for movie: " + BeeMovie.getTicketPrice());
+		System.out.println("Cost for movie: " + EmojiMovie.getTicketPrice());
+		netflixQueue.addMovie(BeeMovie);
+		netflixQueue.addMovie(EmojiMovie);
+		netflixQueue.addMovie(LegoNinjagoMovie);
+		netflixQueue.addMovie(LegoMovie);
+		netflixQueue.addMovie(LegoBatmanMovie);
+		System.out.println("Your Movie List Contains: " + BeeMovie + ", " + EmojiMovie + ", " + LegoBatmanMovie + ", "
+				+ LegoMovie + ", " + LegoNinjagoMovie + " *Whispers* I don't know why you have such taste");
+		System.out.println("The best movie is: " + netflixQueue.getBestMovie());
+		netflixQueue.sortMoviesByRating();
+		System.out.println("The second best movie is: " + netflixQueue.getMovie(1));
 	}
+
 }
